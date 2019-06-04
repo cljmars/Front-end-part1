3dc8f01133980a40b0037ecf1f5d3ff9febcfa92
Liangjun Chu <LChu@sldlaser.com>
2019-06-04 16:21:15 -0700

    Last version, add animate effect

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,85 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';//click
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'; //click
+const HIDDEN_DETAIL_CLASS = 'hidden-detail'; //Hidden details
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const TINY_EFFECT_CLASS = 'is-tiny';
+const ESC_KEY = 27;
 
 //Step1 Get Thumbnails Array
 function getThumbnailsArray() {
   'use strict';
- var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
- var thumbnailArray = [].slice.call(thumbnails);
- return thumbnailArray;
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
 }
 
 //Step2 Function of chaning detailed pic and its text
 function setDetails(imageUrl, titleText) {
- 'use strict';
- var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
- detailImage.setAttribute('src', imageUrl);
- var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
- detailTitle.textContent = titleText;
+  'use strict';
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute('src', imageUrl);
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
 }
 
 function imageFromThumb(thumbnail) {
- return thumbnail.getAttribute('data-image-url');
+  return thumbnail.getAttribute('data-image-url');
 }
 
-function titleFromThumb(thumbnail){
- return thumbnail.getAttribute('data-image-title');
+function titleFromThumb(thumbnail) {
+  return thumbnail.getAttribute('data-image-title');
 }
 
 function setDetailsFromThumb(thumbnail) {
- 'use strict';
- setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
 //Step3 Adding handler for click on thumb pic to change
 function addThumbClickHandler(thumb) {
- 'use strict';
- thumb.addEventListener('click', function (event) {
- event.preventDefault();
- setDetailsFromThumb(thumb)
- });
+  'use strict';
+  thumb.addEventListener('click', function(event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+  });
+}
+
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if (event.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
 }
 
 //Step4 Add initialization function with iterations
 function initializeEvents() {
- 'use strict';
- var thumbnails = getThumbnailsArray();
- thumbnails.forEach(addThumbClickHandler)
+  'use strict';
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler)
+  addKeyPressHandler()
 }
+
+
 
 //Step5 Call Function
 initializeEvents()
